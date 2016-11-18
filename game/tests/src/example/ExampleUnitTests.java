37f3f8e626d4595ff8bd5d796886eb516e44d955
Jason Mashinchi <jason@mashinchi.com>
2016-11-18 00:04:38 +0000

    Added initial version of tests

---
+++ game/tests/src/example/ExampleUnitTests.java
@@ -0,0 +1,18 @@
+package example;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ExampleUnitTests {
+
+    @Test
+    public void passingTest() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    public void failingTest() {
+        //assertEquals(1, 2);
+    }
+}
