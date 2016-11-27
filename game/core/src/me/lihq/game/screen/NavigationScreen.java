f4c9f8bf94195325695d5cbe231801cf9d4a8490
Brooke Hatton <hatton.brooke@gmail.com>
2016-11-27 15:55:39 +0000

    Revert "Merge remote-tracking branch 'refs/remotes/origin/development' into joe-adding-map"
    This reverts commit 0cdfdbfc99f041dfb2cb1b9c70d83c752e8c416d, reversing
    changes made to c38377225d57b5735c8319d9db096c4022951659.

--- game/core/src/me/lihq/game/screen/NavigationScreen.java
+++ game/core/src/me/lihq/game/screen/NavigationScreen.java
@@ -2,7 +2,6 @@ package me.lihq.game.screen;
 
 
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import me.lihq.game.GameMain;
 import com.badlogic.gdx.maps.tiled.TiledMap;
@@ -19,93 +18,102 @@ import me.lihq.game.living.Player;
  * This is the screen that is responsible for the navigation of the player around the game.
  * It displays the current room that the player is in, and allows the user to move the player around between rooms.
  */
-public class NavigationScreen extends AbstractScreen {
+public class NavigationScreen extends AbstractScreen
+{
 
     private TiledMap map;
     private OrthogonalTiledMapRenderer tiledMapRenderer;
-    private OrthographicCamera camera = new OrthographicCamera();
+    private OrthographicCamera camera;
     private Viewport viewport;
+    private Player player;
     private PlayerController playerController;
-    private SpriteBatch spriteBatch;
 
     //TODO: add more information about this class
+
     /**
      * Initialises the navigation screen
+     *
      * @param game
      */
-    public NavigationScreen(GameMain game) {
+    public NavigationScreen(GameMain game)
+    {
         super(game);
 
         float w = Gdx.graphics.getWidth();
         float h = Gdx.graphics.getHeight();
-        camera.setToOrtho(false,w,h);
+
+        Assets.load();
+        //TODO: Consider where we have the camera here or GameMain
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, w, h);
         camera.update();
 
-        viewport = new FitViewport(w/Settings.ZOOM, h/Settings.ZOOM, camera);
-
-
+        viewport = new FitViewport(w, h, camera);
 
         map = new TmxMapLoader().load("map.tmx");
         tiledMapRenderer = new OrthogonalTiledMapRenderer(map);
 
-        playerController = new PlayerController(game.player);
+        //TODO: Move player to GameMain
+        player = new Player("Test name", "player.png");
 
-        spriteBatch = new SpriteBatch();
+        player.setX(10);
+        player.setY(10);
 
+        playerController = new PlayerController(player);
     }
 
     /**
      * This is ran when the navigation screen becomes the visible screen in GameMain
      */
     @Override
-    public void show() {
+    public void show()
+    {
         Gdx.input.setInputProcessor(playerController);
     }
 
-    /** Called when the screen should render itself.
-	 * @param delta The time in seconds since the last render.
+    /**
+     * Called when the screen should render itself.
+     *
+     * @param delta The time in seconds since the last render.
      */
     @Override
-    public void render(float delta) {
-        camera.position.x = game.player.getX();
-        camera.position.y = game.player.getY();
+    public void render(float delta)
+    {
+        camera.position.x = player.getX() * Settings.TILE_SIZE;
+        camera.position.y = player.getY() * Settings.TILE_SIZE;
         camera.update();
-
         tiledMapRenderer.setView(camera);
         tiledMapRenderer.render();
-
-        spriteBatch.setProjectionMatrix(camera.combined);
-        //place Sprites to be drawn in the sprite batch
-        spriteBatch.begin();
-        game.player.draw(spriteBatch);
-        spriteBatch.end();
-
-
 
     }
 
     @Override
-    public void resize(int width, int height) {
+    public void resize(int width, int height)
+    {
         viewport.update(width, height);
     }
 
     @Override
-    public void pause() {
+    public void pause()
+    {
 
     }
 
     @Override
-    public void resume() {
+    public void resume()
+    {
 
     }
 
     @Override
-    public void hide() {
+    public void hide()
+    {
 
     }
 
     @Override
-    public void dispose() {
+    public void dispose()
+    {
         map.dispose();
         tiledMapRenderer.dispose();
     }
