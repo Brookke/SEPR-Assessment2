6762f9d1c2d4c1d5faa32505d65f38ca54bd8adb
beno11224 <beno11224@gmail.com>
2016-11-30 16:55:42 +0000

    Continuous player movement start

--- game/core/src/me/lihq/game/living/controller/PlayerController.java
+++ game/core/src/me/lihq/game/living/controller/PlayerController.java
@@ -17,17 +17,49 @@ public class PlayerController extends InputAdapter
         this.player = player;
     }
 
+    //could put in a class of its own for neatness?
+    public boolean moveLeft = false;
+    public boolean moveRight = false;
+    public boolean moveUp = false;
+    public boolean moveDown = false;
+
     @Override
     public boolean keyDown(int keycode)
     {
         if (keycode == Input.Keys.LEFT || keycode == Input.Keys.A)
-            player.move(-1, 0);
+            moveLeft = true;
+            moveRight = false; //decide whether to remove or not (so only the most recently pressed key is acted upon)
+            moveDown = false;
+            moveUp = false;
         if (keycode == Input.Keys.RIGHT || keycode == Input.Keys.D)
-            player.move(1, 0);
+            moveRight = true;
+            moveLeft = false;
+            moveDown = false;
+            moveUp = false;
         if (keycode == Input.Keys.UP || keycode == Input.Keys.W)
-            player.move(0, 1);
+            moveUp = true;
+            moveLeft = false;
+            moveRight = false;
+            moveDown = false;
         if (keycode == Input.Keys.DOWN || keycode == Input.Keys.S)
-            player.move(0, -1);
+            moveDown = true;
+            moveLeft = false;
+            moveRight = false;
+            moveUp = false;
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode)
+    {
+        if (keycode == Input.Keys.LEFT || keycode == Input.Keys.A)
+            moveLeft = false;
+        if (keycode == Input.Keys.RIGHT || keycode == Input.Keys.D)
+            moveRight = false;
+        if (keycode == Input.Keys.UP || keycode == Input.Keys.W)
+            moveUp = false;
+        if (keycode == Input.Keys.DOWN || keycode == Input.Keys.S)
+            moveDown = false;
         return false;
     }
 }
