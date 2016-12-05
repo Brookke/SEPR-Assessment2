80c3ac6c347ed4c87ab48c5d640765810e8fd6ef
beno11224 <beno11224@gmail.com>
2016-12-05 18:04:55 +0000

    Small changes

--- game/core/src/me/lihq/game/screen/SpeechViewScreen.java
+++ game/core/src/me/lihq/game/screen/SpeechViewScreen.java
@@ -1,8 +1,8 @@
 package me.lihq.game.screen;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import me.lihq.game.GameMain;
@@ -17,30 +17,46 @@ public class SpeechViewScreen extends AbstractScreen {
 
     private Viewport viewport;
     private PlayerController playerController;
-    private OrthographicCamera camera = new OrthographicCamera(); //check which camera will be easiest
+    private PerspectiveCamera camera = new PerspectiveCamera(); //check which camera will be easiest
+    private String PersonTalking = "";
+    private SpriteBatch spriteBatch;
 
     public SpeechViewScreen(GameMain game)
     {
         super(game);
 
-        menuController = new MenuController(game.menu); //need to change from 'playerController' to 'menuController'
+        //menuController = new MenuController(game.menu); //need to change from 'playerController' to 'menuController'
 
         float w = Gdx.graphics.getWidth();
         float h = Gdx.graphics.getHeight();
-        camera.setToOrtho(false,w,h);
         camera.update();
         viewport = new FitViewport(w/ Settings.ZOOM, h/Settings.ZOOM, camera);
+
+        spriteBatch = new SpriteBatch();
+    }
+
+    public void setPersonTalking(String NPCTalking) //must call this func so that the name and picture are correct.
+    {
+        PersonTalking = NPCTalking;
     }
 
     @Override
     public void show()
     {
-        Gdx.input.setInputProcessor(menuController);
+        Gdx.input.setInputProcessor(null);
     }
 
     @Override
     public void render(float delta){
+        camera.position.x = 0;//check (0,0) works ok
+        camera.position.y = 0;
 
+        spriteBatch.setProjectionMatrix(camera.combined);
+        spriteBatch.begin();
+        game.player.draw(spriteBatch);
+        spriteBatch.end();
+        //place image 1 and image 2 - do i make 2 separate classes?
+        //place text box - needs a new class?
     }
 
     @Override
@@ -50,7 +66,7 @@ public class SpeechViewScreen extends AbstractScreen {
     }
 
     @Override
-    public void pause() {
+    public void pause() { //does the speech view need pause?
 
     }
 
@@ -66,6 +82,7 @@ public class SpeechViewScreen extends AbstractScreen {
 
     @Override
     public void dispose() {
+        //just 'dispose' everything
 
     }
 }
